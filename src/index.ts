85cd781b54569cfc1742b1a6521f13be9ddbe438
Mitrix <kremencov.r2d2@gmail.com>
2023-07-14 17:09:11 +0700

    Styles extracted to separate file, modified webpack conf

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { words } from './data/words';
 import { TrainerView } from './trainerView';
 import { TrainerModel } from './trainerModel';
+import './styles.css';
 
 const model = new TrainerModel(words, 6);
 const view = new TrainerView(model);
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
